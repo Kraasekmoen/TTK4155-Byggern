70d0dc955a2a552885e43494f5fea65df5b8fc77
ovrseen <59832519+Kraasekmoen@users.noreply.github.com>
2021-10-15 19:39:59 +0200

    Added a bunch of functions to driver
    But still no success in making the screen do as I want, plus now the entire program won't run? Perhaps we've overloaded the program memory...

--- EX4-2/OLED_EX4/OLED_Driver.c
+++ EX4-2/OLED_EX4/OLED_Driver.c
@@ -9,15 +9,77 @@
 	0x1200-0x13FF:	Command Register
 */
 
-	#define CMD_OFFSET 0x1200;	
-	#define DAT_OFFSET 0x1000;
+	#define CMD_OFFSET 0x1000;	
+	#define DAT_OFFSET 0x1200;
 	
-	//enum addr_mode{Horizontal=0x00, Vertical=0x01, Page=0x02};
+	volatile char *cmd_reg_pointer = CMD_OFFSET;
+	volatile char *gddram_pointer = DAT_OFFSET;
+	
+	volatile oled_cursor_pos Position;
 
+// Fundamental functions
 	
+	// Write data to the command register
 	void oled_cmdreg_write(uint8_t data){
-		volatile uint8_t *memspace_pointer = CMD_OFFSET;
-		*memspace_pointer = data;
+		cmd_reg_pointer[0] = data;
+	}
+	
+	// Write data to the graphic display data ram
+	void oled_gddram_write(uint8_t data){
+		gddram_pointer[0] = data;
+	}
+	
+	void oled_goto_page(uint8_t page){	//Wrapper for set_page_address_range
+		// Check function bounds
+		if (page < 0 || page > 7) { printf("OLED Error: Attempting to go to page out of bounds\n"); return;}
+		set_page_address_range(page, 7);
+		Position.page = page;
+	}
+	
+	void oled_goto_column(uint8_t col){ //Wrapper for set_column_address_range
+		// Check function bounds
+		if (col < 0 || col > 127) { printf("OLED Error: Attempting to go to column out of bounds\n"); return;}
+		set_column_address_range(col, 127);	
+		Position.col = col;
+	}
+	
+	void oled_set_cursor_pos(uint8_t row,uint8_t column){ //Wrapper for goto-functions
+		oled_goto_page(row);
+		oled_goto_column(column);
+	}
+
+	// Set all pixels in page to zero
+	void oled_clear_page(uint8_t page){
+		// Set cursor to beginning of page
+		oled_set_cursor_pos(page, 0);
+		// Clear each column in page
+		for (int column = 0; column <= 127; column++){
+			oled_gddram_write(0b00000000);
+		}
+		// Return cursor to beginning of page
+		oled_set_cursor_pos(page, 0);
+	}
+	
+	void oled_cursor_home(){ //Wrapper for set_cursor_pos
+		oled_set_cursor_pos(0,0);
+	}
+	
+	// Set all pixels on screen to zero
+	void oled_reset(){
+		for (uint8_t page = 0; page < 8; page++){
+			oled_clear_page(page);
+		}
+		oled_cursor_home();
+	}
+	
+	void oled_print_large(unsigned char character){
+		uint8_t c = character - 32;
+		uint8_t fontSize = sizeof(font8[c]);
+		
+		for (int i = 0; i < fontSize; i++){
+			oled_gddram_write(pgm_read_word(&font8[c][i]));
+			Position.col += fontSize;
+		}
 	}
 	
 	void oled_init(){
@@ -45,18 +107,7 @@
 		oled_cmdreg_write(0xaf);	// Display ON
 	}
 	
-	void oled_print_arrow(){
-		oled_gddram_write(0b00011000);
-		oled_gddram_write(0b00011000);
-		oled_gddram_write(0b01111110);
-		oled_gddram_write(0b00111100);
-		oled_gddram_write(0b00011000);
-	}
-	
-	void oled_gddram_write(uint8_t data){
-		volatile uint8_t *memspace_pointer = DAT_OFFSET;
-		*memspace_pointer = data;
-	}
+// Option functions
 	
 	void set_addressing_mode(addr_mode mode){
 		oled_cmdreg_write(0x20);	// Set Memory Addressing Mode
@@ -65,7 +116,11 @@
 	
 	// Only works in horizontal or vertical address modes. To reset, send 0d and 127d
 	void set_column_address_range(uint8_t start, uint8_t end){
-		if (start < end)
+		
+		// Set address mode to horizontal, just to be sure
+		set_addressing_mode(Horizontal);
+		
+		if (start <= end)
 		{
 			oled_cmdreg_write(0x21);
 			oled_cmdreg_write(start);
@@ -78,7 +133,11 @@
 	}
 	// Only works in horizontal or vertical address modes. To reset, send 0d and 7d
 	void set_page_address_range(uint8_t start, uint8_t end){
-		if (start < end)
+		
+		// Set address mode to horizontal, just to be sure
+		set_addressing_mode(Horizontal);		
+
+		if (start <= end)
 		{
 			oled_cmdreg_write(0x22);
 			oled_cmdreg_write(start);
@@ -89,14 +148,28 @@
 		}
 	}
 
-	void address_range_reset(){
+	void reset_address_range(){
 		set_column_address_range(0,127);
 		set_page_address_range(0,7);
 	}
 	
+
+
+
+// Test functions below
+	
+	void oled_print_arrow(){
+		oled_gddram_write(0b00011000);
+		oled_gddram_write(0b00011000);
+		oled_gddram_write(0b01111110);
+		oled_gddram_write(0b00111100);
+		oled_gddram_write(0b00011000);
+	}
+		
+	
 	void testPrint_font(uint8_t ch){
 		for(int i=0; i < sizeof(font8[ch]); i++){
-			printf("Element of index %d = %d:\n", i, font8[1][i]);
+			printf("Element of index %d = %d:\n", i, pgm_read_word(&font8[ch][i]));
 		}
 	}
 	
--- EX4-2/OLED_EX4/OLED_Driver.h
+++ EX4-2/OLED_EX4/OLED_Driver.h
@@ -7,6 +7,7 @@
 
 
 	typedef enum {Horizontal=0x00, Vertical=0x01, Page=0x02}addr_mode;
+	typedef struct {uint8_t col, page; } oled_cursor_pos;
 
 	void oled_cmdreg_write(uint8_t data);
 	void oled_gddram_write(uint8_t data);
@@ -14,11 +15,11 @@
 	void oled_init();
 	void oled_reset();
 	void oled_home();
-	void oled_goto_line();
-	void oled_goto_comlumn();
-	void oled_clear_line();
-	void oled_pos(uint8_t row,uint8_t column);
-	void oled_print(uint8_t * character);
+	void oled_goto_page(uint8_t page);
+	void oled_goto_column(uint8_t col);
+	void oled_clear_page(uint8_t page);
+	void oled_set_cursor_pos(uint8_t row,uint8_t column);
+	void oled_print_large(unsigned char character);
 
 	void set_addressing_mode(addr_mode mode);
 	
